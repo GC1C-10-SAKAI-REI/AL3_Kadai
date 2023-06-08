1b002923a312cea434cf9acb171a29d6d5beec53
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-06-08 14:49:47 +0900

    プレイヤーモデルのサイズ調整

--- Player.cpp
+++ Player.cpp
@@ -23,7 +23,14 @@ void Player::Initialize(ViewProjection viewProjection)
 
 void Player::Update()
 {
+	//変換行列を更新
+	playerWorldTransform_.matWorld_ = MakeAffineMatrix(
+	    playerWorldTransform_.scale_,
+		playerWorldTransform_.rotation_,
+	    playerWorldTransform_.translation_);
 
+	//変換行列を定数バッファに転送
+	playerWorldTransform_.TransferMatrix();
 }
 
 void Player::Draw3D()
